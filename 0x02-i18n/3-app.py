417790e80fec63113f8b0ef0fc4bcfc925a980f2
hassanabdisalan <hassanabdisalan3@gmail.com>
2025-05-20 17:54:22 +0300

    Task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,25 +1,46 @@
-from flask import Flask, render_template, request
-from flask_babel import Babel, gettext as _
+#!/usr/bin/env python3
+"""
+This module creates a Flask app.
+"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+from flask_babel import Babel, _
+from typing import List
 
-app = Flask(__name__)
 
 class Config:
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    """
+    This class configures available languages in our app.
+    """
+    LANGUAGES: List[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
 
+
+app: Flask = Flask(__name__)
+babel: Babel = Babel(app)
 app.config.from_object(Config)
-babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """
+    This function determines the best match with
+    our supported languages.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
 
 @app.route('/')
-def index():
-    return render_template('3-index.html', 
-                           home_title=_("home_title"), 
-                           home_header=_("home_header"))
+def index() -> str:
+    """
+    This function returns a template.
+    """
+    return render_template('3-index.html')
+
 
 if __name__ == "__main__":
     app.run()
