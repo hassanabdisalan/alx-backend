53a8f6b10d84d96fb1a2ba9db13263e6a039dbd8
hassanabdisalan <hassanabdisalan3@gmail.com>
2025-05-20 17:50:37 +0300

    Task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext as _
+
+app = Flask(__name__)
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index():
+    return render_template('3-index.html', 
+                           home_title=_("home_title"), 
+                           home_header=_("home_header"))
+
+if __name__ == "__main__":
+    app.run()
