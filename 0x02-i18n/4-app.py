50c5c7161440a9c0f63d95718e5f5ab814da4108
hassanabdisalan <hassanabdisalan3@gmail.com>
2025-05-20 17:56:58 +0300

    Task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Flask app with forced locale via query parameter"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'fr']
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    # Check if the locale is specified in the query parameter
+    locale = request.args.get('locale')
+    if locale in app.config['BABEL_SUPPORTED_LOCALES']:
+        return locale
+    return request.accept_languages.best_match(app.config['BABEL_SUPPORTED_LOCALES'])
+
+@app.route('/')
+def index():
+    return render_template('4-index.html')
